e8c6f03d56d4bde7fea4b264f104f75c50a8d444
peter <peter135268@gmail.com>
2023-02-17 15:37:33 +0900

    Basic Frame designed

--- basicframe/urls.py
+++ basicframe/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
+    path('advertisements/', include('advertisementapp.urls')),
 ] +static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
